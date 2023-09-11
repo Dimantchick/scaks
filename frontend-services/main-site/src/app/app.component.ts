f355d1fbbb4f6e0cc243d77dfdb86a05b224d6d0
dmitrii.reshetinskii <dmitrii.reshetinskii@domru.ru>
2023-09-11 18:58:32 +0700

    Logout url from environment

--- frontend-services/main-site/src/app/app.component.ts
+++ frontend-services/main-site/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {KeycloakProfile} from "keycloak-js";
 import {KeycloakService} from "keycloak-angular";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -33,6 +34,6 @@ export class AppComponent implements OnInit  {
   }
 
   public logout() {
-    this.keycloak.logout("https://scaks.dimantchick.tk");
+    this.keycloak.logout(environment.FRONTEND_URL);
   }
 }
